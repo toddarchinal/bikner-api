3ac31d55e1804dc6cf72db01b200533db04986e3
Todd Archinal <toddarchinal@gmail.com>
2019-02-06 21:39:01 -0500

    added questions

--- app/Http/Controllers/PollsController.php
+++ app/Http/Controllers/PollsController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Poll;
+use Validator;
 use Illuminate\Http\Request;
+use App\Http\Resources\PollResource;
+
+//use App\Http\Resources\Poll as PollResource; //should rename class as PollResource
 
 class PollsController extends Controller
 {
@@ -13,10 +17,24 @@ class PollsController extends Controller
     }
     public function show($id)
     {
-        return response()->json(Poll::find($id), 200);
+        $poll = Poll::find($id);
+        if(is_null($poll)){
+            return response()->json(null, 404);
+        }
+        $response = new PollResource($poll); 
+        return response()->json($response, 200);
     }
     public function store(Request $request)
     {
+        $rules =[
+            'title' => 'required|max:10',
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        
+        if($validator->fails()){
+            return response()->json($validator->errors(), 400);
+        }
+
         $poll = Poll::create($request->all());
         return response()->json($poll, 201);
     }
@@ -29,5 +47,14 @@ class PollsController extends Controller
     {
         $poll->delete();
         return response()->json(null, 204);
+    }
+    public function errors()
+    {
+        return response()->json(['msg' => 'Payment is required'], 501);
+    }
+    public function questions(Request $request, Poll $poll)         
+    {
+        $questions = $poll->questions;
+        return response()->json($questions, 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,10 @@ Route::get('polls/{id}', 'PollsController@show');
 Route::post('polls', 'PollsController@store');
 Route::put('polls/{poll}', 'PollsController@update');
 Route::delete('polls/{poll}', 'PollsController@delete');
+
+Route::any('errors', 'PollsController@errors');
+
+Route::apiResource('questions', 'QuestionsController');
+
+Route::get('polls/{poll}/questions', 'PollsController@questions');
+
